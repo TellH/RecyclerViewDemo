adf5ae87eb1a02fa5b67032295ef44bcb1220a83
TellH <570495627@qq.com>
2016-03-20 11:40:46 +0800

    PopupMenu的高级用法+

---
+++ app/src/main/java/com/example/tellh/recyclerviewdemo/activity/PopupMenuActivity.java
@@ -0,0 +1,135 @@
+package com.example.tellh.recyclerviewdemo.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.tellh.recyclerviewdemo.DividerGridItemDecoration;
+import com.example.tellh.recyclerviewdemo.R;
+import com.example.tellh.recyclerviewdemo.adapter.BaseRecyclerAdapter;
+import com.example.tellh.recyclerviewdemo.adapter.RecyclerViewHolder;
+import com.example.tellh.recyclerviewdemo.listener.OnShowPopupMenuClickListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PopupMenuActivity extends AppCompatActivity {
+
+    List<String> mDataList;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        switch (id) {
+            case R.id.id_action_gridview:
+                recyclerView.addItemDecoration(new DividerGridItemDecoration(this));
+                recyclerView.setLayoutManager(new GridLayoutManager(this,3));
+                break;
+            case R.id.id_action_listview:
+                recyclerView.setLayoutManager(new LinearLayoutManager(this));
+                break;
+            case R.id.id_action_horizontalGridView:
+                recyclerView.setLayoutManager(new StaggeredGridLayoutManager(4,StaggeredGridLayoutManager.HORIZONTAL));
+                break;
+            case R.id.id_action_staggeredgridview:
+                startActivity(new Intent(this,StaggeredActivity.class));
+                break;
+            case R.id.id_action_add:
+                ((BaseRecyclerAdapter)mAdapter).add(2, "new item");
+                break;
+            case R.id.id_action_delete:
+                ((BaseRecyclerAdapter)mAdapter).delete(2);
+                break;
+            default:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void init() {
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        mDataList = new ArrayList<>();
+        for (int i = 0; i <= 30; i++) {
+            mDataList.add(String.valueOf(i));
+        }
+        //设置item动画
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        mAdapter = new BaseRecyclerAdapter<String>(this,mDataList) {
+            @Override
+            protected int getItemLayoutId(int viewType) {
+                return R.layout.item_cardview;
+            }
+            @Override
+            protected void bindData(RecyclerViewHolder holder, int position,String item) {
+                //调用holder.getView(),getXXX()方法根据id得到控件实例，进行数据绑定即可
+                holder.setText(R.id.tv_num,item);
+                holder.getView(R.id.iv_reorder).setOnClickListener(new OnShowPopupMenuClickListener(PopupMenuActivity.this,recyclerView,holder));
+            }
+        };
+        recyclerView.setAdapter(mAdapter);
+        //添加item点击事件监听
+        ((BaseRecyclerAdapter)mAdapter).setOnItemClickListener(new BaseRecyclerAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View itemView, int pos) {
+                Toast.makeText(PopupMenuActivity.this, "click " + pos, Toast.LENGTH_SHORT).show();
+            }
+        });
+        ((BaseRecyclerAdapter)mAdapter).setOnItemLongClickListener(new BaseRecyclerAdapter.OnItemLongClickListener() {
+            @Override
+            public void onItemLongClick(View itemView, int pos) {
+                Toast.makeText(PopupMenuActivity.this, "long click " + pos, Toast.LENGTH_SHORT).show();
+            }
+        });
+        //设置布局样式LayoutManager
+        recyclerView.setLayoutManager(new LinearLayoutManager(PopupMenuActivity.this, LinearLayoutManager.VERTICAL, false));
+//        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 3);
+//        recyclerView.setLayoutManager(gridLayoutManager);
+//        ((BaseRecyclerAdapter) mAdapter).getGridLayoutManager(gridLayoutManager);
+    }
+}
---
+++ app/src/main/java/com/example/tellh/recyclerviewdemo/listener/OnShowPopupMenuClickListener.java
@@ -0,0 +1,171 @@
+package com.example.tellh.recyclerviewdemo.listener;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.support.v7.view.menu.MenuBuilder;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.PopupMenu;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.widget.Toast;
+
+import com.example.tellh.recyclerviewdemo.R;
+import com.example.tellh.recyclerviewdemo.adapter.RecyclerViewHolder;
+
+import java.lang.reflect.Field;
+
+/**
+ * Created by tlh on 2016/3/20.
+ */
+public class OnShowPopupMenuClickListener implements View.OnClickListener {
+    private static final String TAG = "OnShowPopupMenuClickListener";
+    private final RecyclerView mRecyclerView;
+    private final int mPosition;
+    private final RecyclerViewHolder mHolder;
+    //    private Album mAlbum;
+    private Context mContext;
+    private final AlphaAnimation mFadeOut = new AlphaAnimation(1.0f, 0.3f);
+    private final AlphaAnimation mFadeIn = new AlphaAnimation(0.3f, 1.0f);
+    public OnShowPopupMenuClickListener(Context context, RecyclerView recyclerView, RecyclerViewHolder holder) {
+        mContext = context;
+        mRecyclerView=recyclerView;
+        mPosition=holder.getLayoutPosition();
+        mHolder=holder;
+        mFadeIn.setFillAfter(true);
+        mFadeOut.setFillAfter(true);
+//        mAlbum = album;
+    }
+    @SuppressLint("LongLogTag")
+    @Override
+    public void onClick(View v) {
+        // This is an android.support.v7.widget.PopupMenu;
+        PopupMenu popupMenu = new PopupMenu(mContext, v) {
+            @Override
+            public boolean onMenuItemSelected(MenuBuilder menu, MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.popup_delete:
+//                        deleteAlbum(mAlbum);
+                        Toast.makeText(mContext, "you click delete", Toast.LENGTH_SHORT).show();
+                        return true;
+
+                    case R.id.popup_rename:
+//                        renameAlbum(mAlbum);
+                        Toast.makeText(mContext, "you click rename", Toast.LENGTH_SHORT).show();
+                        return true;
+
+                    case R.id.popup_lock:
+//                        lockAlbum(mAlbum);
+                        Toast.makeText(mContext, "you click lock", Toast.LENGTH_SHORT).show();
+                        return true;
+
+                    case R.id.popup_unlock:
+//                        unlockAlbum(mAlbum);
+                        Toast.makeText(mContext, "you click unlock", Toast.LENGTH_SHORT).show();
+                        return true;
+
+                    case R.id.popup_set_cover:
+//                        setAlbumCover(mAlbum);
+                        Toast.makeText(mContext, "you click set cover", Toast.LENGTH_SHORT).show();
+                        return true;
+
+                    default:
+                        return super.onMenuItemSelected(menu, item);
+                }
+            }
+        };
+
+        popupMenu.inflate(R.menu.popup_menu);
+
+//        if (mAlbum.isLocked()) {
+//            popupMenu.getMenu().removeItem(R.id.album_overflow_lock);
+//            popupMenu.getMenu().removeItem(R.id.album_overflow_rename);
+//            popupMenu.getMenu().removeItem(R.id.album_overflow_delete);
+//        } else {
+//            popupMenu.getMenu().removeItem(R.id.album_overflow_unlock);
+//        }
+
+        // Force icons to show
+        Object menuHelper;
+        Class[] argTypes;
+        try {
+            //PopupMenu有一个私有变量mPopup，类型是MenuPopupHelper，有一个方法setForceShowIcon，用于显示icon
+            Field fMenuHelper = PopupMenu.class.getDeclaredField("mPopup");
+            fMenuHelper.setAccessible(true);
+            menuHelper = fMenuHelper.get(popupMenu);
+            argTypes = new Class[] { boolean.class };
+            menuHelper.getClass().getDeclaredMethod("setForceShowIcon", argTypes).invoke(menuHelper, true);
+        } catch (Exception e) {
+            // Possible exceptions are NoSuchMethodError and NoSuchFieldError
+            //
+            // In either case, an exception indicates something is wrong with the reflection code, or the
+            // structure of the PopupMenu class or its dependencies has changed.
+            //
+            // These exceptions should never happen since we're shipping the AppCompat library in our own apk,
+            // but in the case that they do, we simply can't force icons to display, so log the error and
+            // show the menu normally.
+
+            Log.w(TAG, "error forcing menu icons to show", e);
+            popupMenu.show();
+            return;
+        }
+        popupMenu.show();
+
+        //调整popupMenu的位置偏移量
+        try {
+            Field fListPopup = menuHelper.getClass().getDeclaredField("mPopup");
+            fListPopup.setAccessible(true);
+            Object listPopup = fListPopup.get(menuHelper);
+            argTypes = new Class[] { int.class };
+            Class listPopupClass = listPopup.getClass();
+
+            // Get the width of the popup window
+            int width = (Integer) listPopupClass.getDeclaredMethod("getWidth").invoke(listPopup);
+
+            // Invoke setHorizontalOffset() with the negative width to move left by that distance
+            listPopupClass.getDeclaredMethod("setHorizontalOffset", argTypes).invoke(listPopup, -width+100);
+
+            // Invoke show() to update the window's position
+            listPopupClass.getDeclaredMethod("show").invoke(listPopup);
+        } catch (Exception e) {
+            // Again, an exception here indicates a programming error rather than an exceptional condition
+            // at runtime
+            Log.w(TAG, "Unable to force offset", e);
+        }
+        FadingOutOtherItems(popupMenu);
+    }
+
+    private void FadingOutOtherItems(PopupMenu popupMenu) {
+        // Dim out all the other list items if they exist
+        LinearLayoutManager layoutManager = (LinearLayoutManager) mRecyclerView.getLayoutManager();
+        int firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition();
+        int lastVisibleItemPosition = layoutManager.findLastVisibleItemPosition();
+        for (int i=firstVisibleItemPosition;i<=lastVisibleItemPosition;i++){
+            if (i==mHolder.getLayoutPosition()) continue;
+            RecyclerViewHolder viewHolder = (RecyclerViewHolder) mRecyclerView.findViewHolderForLayoutPosition(i);
+            View itemView = viewHolder.getItemView();
+            itemView.clearAnimation();
+            itemView.startAnimation(mFadeOut);
+        }
+
+        // Make sure to bring them back to normal after the menu is gone
+        popupMenu.setOnDismissListener(new PopupMenu.OnDismissListener() {
+            @Override
+            public void onDismiss(PopupMenu popupMenu) {
+                LinearLayoutManager layoutManager = (LinearLayoutManager) mRecyclerView.getLayoutManager();
+                int firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition();
+                int lastVisibleItemPosition = layoutManager.findLastVisibleItemPosition();
+                for (int i=firstVisibleItemPosition;i<=lastVisibleItemPosition;i++){
+                    if (i== mHolder.getLayoutPosition()) continue;
+                    RecyclerViewHolder viewHolder = (RecyclerViewHolder) mRecyclerView.findViewHolderForLayoutPosition(i);
+                    View itemView = viewHolder.getItemView();
+                    viewHolder.getLayoutPosition();
+                    itemView.clearAnimation();
+                    itemView.startAnimation(mFadeIn);
+                }
+            }
+        });
+    }
+}
